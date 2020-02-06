f8a68adf289c054616aa241182e1b8d23c57b075
mmadariaga <mikel@irontec.com>
2020-02-06 12:19:05 +0100

    ensure ObjectHydrator triggers HydratorComplete on foreign entities

--- Infrastructure/Persistence/Doctrine/Hydration/ObjectHydrator.php
+++ Infrastructure/Persistence/Doctrine/Hydration/ObjectHydrator.php
@@ -3,33 +3,69 @@
 namespace Ivoz\Core\Infrastructure\Persistence\Doctrine\Hydration;
 
 use Doctrine\ORM\Internal\Hydration\ObjectHydrator as DoctrineObjectHydrator;
-use Ivoz\Core\Infrastructure\Persistence\Doctrine\Events;
+use Ivoz\Core\Infrastructure\Persistence\Doctrine\Events as IvozEvents;
 use Doctrine\Common\Persistence\Event\LifecycleEventArgs;
+use Doctrine\ORM\Events;
 
 class ObjectHydrator extends DoctrineObjectHydrator
 {
+    protected $loadedEntities = [];
+
     /**
      * {@inheritdoc}
      */
     public function hydrateAll($stmt, $resultSetMapping, array $hints = array())
     {
+        $evm = $this->_em->getEventManager();
+        $evm->addEventListener(
+            [Events::postLoad],
+            $this
+        );
+
         $response = parent::hydrateAll(...func_get_args());
 
-        if (empty($response)) {
-            return $response;
+        $evm->removeEventListener(
+            [Events::postLoad],
+            $this
+        );
+
+        $mustTriggerEvents =
+            !empty($response)
+            && is_object($response[0]);
+
+        if ($mustTriggerEvents) {
+            $reponseClass = get_class($response[0]);
+            $foreignEntities = array_filter(
+                $this->loadedEntities,
+                function ($entity) use ($reponseClass) {
+                    return !($entity instanceof $reponseClass);
+                }
+            );
+
+            $this->triggerHydratorCompleteEvent($foreignEntities);
+            $this->triggerHydratorCompleteEvent($response);
         }
 
+        $this->loadedEntities = [];
+        return $response;
+    }
+
+    public function postLoad(LifecycleEventArgs $args)
+    {
+        $this->loadedEntities[] = $args->getObject();
+    }
+
+    protected function triggerHydratorCompleteEvent(array $entities)
+    {
         $evm = $this->_em->getEventManager();
-        foreach ($response as $entity) {
+        foreach ($entities as $entity) {
             $evm->dispatchEvent(
-                Events::onHydratorComplete,
+                IvozEvents::onHydratorComplete,
                 new LifecycleEventArgs(
                     $entity,
                     $this->_em
                 )
             );
         }
-
-        return $response;
     }
 }
