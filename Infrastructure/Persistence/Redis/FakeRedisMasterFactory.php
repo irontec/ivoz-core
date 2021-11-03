875b56f4d6a09a61db6a4e30e684549c31a57134
Mikel Madariaga <mikel@irontec.com>
2021-11-03 08:41:11 +0100

    backport RedisMasterFactory from 4.x

---
+++ Infrastructure/Persistence/Redis/FakeRedisMasterFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Ivoz\Core\Infrastructure\Persistence\Redis;
+
+class FakeRedisMasterFactory extends RedisMasterFactory
+{
+    public function create(int $dbIndex = 1): \Redis
+    {
+        $fakeRedis = new Class extends \Redis {
+            public function connect(
+                $host,
+                $port = 6379,
+                $timeout = 0.0,
+                $reserved = null,
+                $retryInterval = 0,
+                $readTimeout = 0.0
+            ) {}
+
+            public function lPush($key, ...$value1) {}
+
+            public function rPush($key, ...$value1) {}
+
+            public function blPop($key, $timeout_or_key, ...$extra_args) {}
+        };
+
+        return new $fakeRedis;
+    }
+}
