475377a16668cf80ce6cd896295381014128a5d3
Mikel <mikel@irontec.com>
2022-04-21 16:24:50 +0200

    error prevention in DtoNormalizer

--- Application/Model/DtoNormalizer.php
+++ Application/Model/DtoNormalizer.php
@@ -113,7 +113,7 @@ trait DtoNormalizer
         foreach ($contextProperties as $key => $value) {
             if (is_array($value)) {
                 foreach ($value as $property) {
-                    if (!isset($data[$key]) || !array_key_exists($property, $data[$key])) {
+                    if (!isset($data[$key]) || !is_string($property) || !array_key_exists($property, $data[$key])) {
                         continue;
                     }
 
